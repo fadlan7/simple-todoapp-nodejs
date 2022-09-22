7602c1cb6e052f45b291f17ccb13ef473fd5c20e
unknown <inifadlan7@gmail.com>
2022-09-22 11:50:59 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello world!');
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`app started on port ${PORT}`);
+});
---
+++ src/routes/todos.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//Get all todos
+router.get('/', async (req, res) => {});
+
+//Create a todo
+router.post('/todo', async (req, res) => {});
+
+//Update a todo
+router.put;
